d4ffa80c754e6f2998bbee8805db00eac630a6a6
Alisson Carvalho <webalissoncs@gmail.com>
2014-11-28 21:22:14 -0200

    Exclusão + appr 1

--- src/br/com/arvorebinaria/Arvore.java
+++ src/br/com/arvorebinaria/Arvore.java
@@ -15,7 +15,6 @@ public class Arvore {
 		this.e = this.d = null;
 	}
 	
-	
 	public static int limite = 4,contaNivel = 0;
 	
 	// Controle
@@ -25,13 +24,14 @@ public class Arvore {
 		
 		// Caso exceda o limite da árvore
 		if(contaNivel >= limite){
+			contaNivel = 0;
 			System.out.println("\nLimite excedido no item "+num+"\n");
-			return null;
-		}
+		}else {
 		
 		if(a.num < num) {
 			if(a.d != null) add(a.d,num);
 			else{
+				System.out.println("Número "+num+" inserido com sucesso no nível "+contaNivel);
 				a.d = new Arvore(num,contaNivel);
 				contaNivel=0;
 			}
@@ -39,6 +39,7 @@ public class Arvore {
 		else if(a.num > num) {
 			if(a.e != null) add(a.e,num);
 			else{
+				System.out.println("Número "+num+" inserido com sucesso no nível "+contaNivel);
 				a.e = new Arvore(num,contaNivel);
 				contaNivel=0;
 			}
@@ -46,7 +47,7 @@ public class Arvore {
 			System.out.println("\n Número já existente na árvore no nível" + a.nivel);
 			return null;
 		}
-		
+		}
 		return a;
 	}
 	public static void print(Arvore a){
@@ -57,10 +58,50 @@ public class Arvore {
 		}
 	}
 	
-	public static void delete(Arvore a, int num){
+	public static int aux;
+	
+	public static Arvore delete(Arvore a, int num){
+		System.out.println("Tentando excluir "+num+"...");
 		
-		
+		if(a == null) {
+			System.out.println("- Valor digitado "+num+" não existe");
+			return null;
+		}else{
+		if(a.num > num) {
+			a.e = delete(a.e,num);
+		}else if(a.num < num){
+			a.d = delete(a.d,num);
+		}else if(a.num == num){
+			
+			// Verifica se o elemento possui filhos
+			if(a.d == null && a.e == null){
+				a = null;
+			}else if(a.d == null){
+				a.e.nivel--;
+				a = a.e;
+			}
+			else if(a.e == null){
+				a.d.nivel--;
+				a = a.d;
+			}
+			else {
+				System.out.println("- Número "+num+" possui filhos");
+                Arvore b = a.e;
+                while (b.d != null) {
+                	b.d.nivel--;
+                    b = b.d;
+                }
+                //System.out.println("");
+                a.num = b.num;
+                b.num = num;
+                a.e = delete(a.e, num);
+            }
+			System.out.println("- Número "+num+" excluído com sucesso!");
+		}
+		}
+		return a;
 		
 	}
+	
 	
 }
--- src/br/com/arvorebinaria/Interface.java
+++ src/br/com/arvorebinaria/Interface.java
@@ -16,7 +16,20 @@ public class Interface {
 		System.out.println("[6] Sair\n");
 	}
 	private static int getValor(){
-		return Integer.parseInt(scanner.nextLine());
+		boolean n = false;
+		int p = 0;
+		
+		do{
+			try{
+				p = Integer.parseInt(scanner.nextLine());
+				n = true;
+			}catch(Exception e){
+				System.out.println("Valor inválido");
+				n = false;
+			}
+		}while(n == false);
+		
+		return p;
 	}
 	
 	private static Arvore raiz = null;
@@ -35,32 +48,44 @@ public class Interface {
 			
 			switch(t){
 				case 1:
-					
-					/*/System.out.println("\n Digite o valor para inserir: \n");
+					/*System.out.println("Digite o valor para inserir: ");
 					tmp = getValor();
 					
 					if(raiz == null){
 						raiz = new Arvore(tmp);
+						System.out.println("Número "+tmp+" inserido com sucesso no nível 0");
 					}else{
 						raiz = Arvore.add(raiz,tmp);
-					}*/
-					raiz = new Arvore(11);
-					raiz = Arvore.add(raiz,12);
-					raiz = Arvore.add(raiz,9);
-					raiz = Arvore.add(raiz,28);
-					raiz = Arvore.add(raiz,4);
-					raiz = Arvore.add(raiz,6);
-					raiz = Arvore.add(raiz,0);
-					raiz = Arvore.add(raiz,2);
+					}
+					*/
+					
+					raiz = new Arvore(100);
+					raiz.add(raiz,50);
+					raiz.add(raiz,75);
+					raiz.add(raiz,80);
+					raiz.add(raiz,60);
+					raiz.add(raiz,25);
+					raiz.add(raiz,12);
+					raiz.add(raiz,120);
+					raiz.add(raiz,130);
+					raiz.add(raiz,110);
 					
 					Arvore.print(raiz);
 					
 				break;
 				
 				case 2:
+					System.out.println("\nValor na árvore: \n");
 					Arvore.print(raiz);
 				break;
 				
+				case 3:
+					System.out.println("Digite o valor para excluir: ");
+					tmp = getValor();
+					
+					raiz = Arvore.delete(raiz,tmp);
+					
+				break;
 				
 			
 			}
