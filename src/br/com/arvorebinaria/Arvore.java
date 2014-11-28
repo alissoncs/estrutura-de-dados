c695c1fa52a86f88e4f19665f99f42b09568c551
Alisson Carvalho <webalissoncs@gmail.com>
2014-11-28 00:04:15 -0200

    Criando as classes

---
+++ src/br/com/arvorebinaria/Arvore.java
@@ -0,0 +1,12 @@
+package br.com.arvorebinaria;
+
+public class Arvore {
+	int num,nivel;
+	Arvore e,d;
+	
+	public Arvore(){}
+	public Arvore(int num){
+		this.num = num;
+	}
+	
+}
---
+++ src/br/com/arvorebinaria/Interface.java
@@ -0,0 +1,7 @@
+package br.com.arvorebinaria;
+
+public class Interface {
+	public static void main(String[] args) {
+
+	}
+}
